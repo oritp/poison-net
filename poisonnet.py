80eee4c43ff213b0d2f8ea251e2f618af4eed865
Oriol Torres Pla <100279838+oritp@users.noreply.github.com>
2025-10-07 22:14:38 +0200

    Create poisonnet.py

---
+++ poisonnet.py
@@ -0,0 +1,47 @@
+def main():
+    print("""
+    ===========================
+           Poison Net Tool      
+    ===========================
+    1. ARP spoofing
+    2. DNS spoofing
+    3. ARP and DNS sniffing
+    4. Restore ARP
+    5. Restore DNS
+    6. Exit
+    """)
+    
+    while True:
+        option = input("Select an option: ")
+        if option == "1":
+            target_ip = input("Enter the target IP: ")
+            gateway_ip = input("Enter the router IP: ")
+            interface = get_interface()
+            start_arp_spoofing(target_ip, gateway_ip, interface)
+        elif option == "2":
+            target_ip = input("Enter the target IP: ")
+            gateway_ip = input("Enter the router IP: ")
+            fake_ip = input("Enter the chosen IP: ")
+            interface = get_interface()
+            start_arp_spoofing(target_ip, gateway_ip, interface)
+            start_dns_spoofing(interface, target_ip, fake_ip)
+        elif option == "3":
+            target_ip = input("Enter the target IP: ")
+            gateway_ip = input("Enter the router IP: ")
+            interface = get_interface()
+            start_arp_spoofing(target_ip, gateway_ip, interface)
+            start_sniffing(interface, target_ip)
+        elif option == "4":
+            target_ip = input("Enter the target IP: ")
+            gateway_ip = input("Enter the router IP: ")
+            restore_arp(target_ip, gateway_ip)
+        elif option == "5":
+            restore_dns()
+        elif option == "6":
+            print("Exiting...")
+            break
+        else:
+            print("[!] Invalid option.")
+
+if __name__ == "__main__":
+    main()
