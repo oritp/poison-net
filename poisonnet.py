07c9e5c9a2e04f0582c9eebbff8263cca3b7603a
Oriol Torres Pla <100279838+oritp@users.noreply.github.com>
2025-10-14 12:24:50 +0200

    Update poisonnet.py

--- poisonnet.py
+++ poisonnet.py
@@ -23,13 +23,13 @@ def main():
     while True:
         option = input("Select an option: ")
         if option == "1":
-            print("\nYou choose ARP spoofing.")
+            print("\n[+] You choose ARP spoofing.")
             target_ip = input("Enter the target IP: ")
             gateway_ip = input("Enter the router IP: ")
             interface = get_interface()
             start_arp_spoofing(target_ip, gateway_ip, interface)
         elif option == "2":
-            print("\nYou choose DNS spoofing.")
+            print("\n[+] You choose DNS spoofing.")
             target_ip = input("Enter the target IP: ")
             gateway_ip = input("Enter the router IP: ")
             fake_ip = input("Enter the chosen IP: ")
@@ -37,22 +37,22 @@ def main():
             start_arp_spoofing(target_ip, gateway_ip, interface)
             start_dns_spoofing(interface, target_ip, fake_ip)
         elif option == "3":
-            print("\nYou choose ARP and DNS sniffing.")
+            print("\n[+] You choose ARP and DNS sniffing.")
             target_ip = input("Enter the target IP: ")
             gateway_ip = input("Enter the router IP: ")
             interface = get_interface()
             start_arp_spoofing(target_ip, gateway_ip, interface)
             start_sniffing(interface, target_ip)
         elif option == "4":
-            print("\nYou choose ARP restoring.")
+            print("\n[+] You choose ARP restoring.")
             target_ip = input("Enter the target IP: ")
             gateway_ip = input("Enter the router IP: ")
             restore_arp(target_ip, gateway_ip)
         elif option == "5":
-            print("\nYou choose DNS restoring.")
+            print("\n[+] You choose DNS restoring.")
             restore_dns()
         elif option == "6":
-            print("Exiting... See you soon!\n")
+            print("[+] Exiting... See you soon!\n")
             break
         else:
             print("[!] Invalid option.")
